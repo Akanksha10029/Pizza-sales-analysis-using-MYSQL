8162abe7c8c277b1dc2cebbe877872658277e2fb
Akanksha Rani <112504655+Akanksha10029@users.noreply.github.com>
2024-04-26 00:43:47 +0530

    Add files via upload

---
+++ Retrieval of  total number of orders placed.sql
@@ -0,0 +1,9 @@
+-- Retrieve the total number of orders placed
+
+SELECT 
+    COUNT(order_id) AS Total_order_placed
+FROM
+    orders;
+
+
+
---
+++ selecting queries for all tables.sql
@@ -0,0 +1,7 @@
+SELECT * FROM pizzahub.order_details;
+
+SELECT * FROM pizzahub.pizza_types;
+
+SELECT * FROM pizzahub.pizzas;
+
+SELECT * FROM pizzahub.orders;
