f9e707010755767cff3f1f42de22cffc053a838b
Akanksha Rani <112504655+Akanksha10029@users.noreply.github.com>
2024-04-26 00:46:26 +0530

    Delete distribution of orders by hour of the day.sql

--- distribution of orders by hour of the day.sql
+++
@@ -1,7 +0,0 @@
--- Determine the distribution of orders by hour of the day
- SELECT 
-    HOUR(order_time) AS hour, COUNT(order_id) AS order_counts
-FROM
-    orders
-GROUP BY hour
-ORDER BY order_counts DESC
